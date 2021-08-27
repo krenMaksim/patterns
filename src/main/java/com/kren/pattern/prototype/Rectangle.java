d5cab81092741fab4cd610cb712487738135e5d1
krenMaksim <krenmaba@gmail.com>
2021-08-27 16:18:08 +0300

    rename Prototype

---
+++ src/main/java/com/kren/pattern/prototype/Rectangle.java
@@ -0,0 +1,29 @@
+package com.kren.pattern.prototype;
+
+class Rectangle extends Shape {
+
+    private final int width;
+    private final int height;
+
+    public Rectangle() {
+        height = 0;
+        width = 0;
+    }
+
+    public Rectangle(int x, int y, String color, int width, int height) {
+        super(x, y, color);
+        this.width = width;
+        this.height = height;
+    }
+
+    private Rectangle(Rectangle r) {
+        super(r);
+        width = r.width;
+        height = r.height;
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+}
