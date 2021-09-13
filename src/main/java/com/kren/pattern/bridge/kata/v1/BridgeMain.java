6d62f966ebb5d6d32e25b2835ec9ac24cf88dbbc
krenMaksim <krenmaba@gmail.com>
2021-09-13 10:52:02 +0300

    add src/main/java/com/kren/pattern/bridge/kata/v1/BridgeMain.java

---
+++ src/main/java/com/kren/pattern/bridge/kata/v1/BridgeMain.java
@@ -0,0 +1,10 @@
+package com.kren.pattern.bridge.kata.v1;
+
+class BridgeMain {
+
+    public static void main(String[] args) {
+        // TODO read about this pattern in HeardFirst book
+        // Shapes differentiated by color, type etc.
+    }
+
+}
