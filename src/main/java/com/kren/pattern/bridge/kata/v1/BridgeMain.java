47736c3d892242e642d9ad20d1605d6eda1fdee3
krenMaksim <krenmaba@gmail.com>
2021-09-13 11:04:16 +0300

    finish bridge

--- src/main/java/com/kren/pattern/bridge/kata/v1/BridgeMain.java
+++ src/main/java/com/kren/pattern/bridge/kata/v1/BridgeMain.java
@@ -2,15 +2,31 @@ package com.kren.pattern.bridge.kata.v1;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
 
 class BridgeMain {
 
     public static void main(String[] args) {
         // TODO read about this pattern in HeardFirst book
         // Shapes differentiated by color, type etc.
+
+        var red = new Color("RED");
+        var blue = new Color("BLUE");
+        var green = new Color("GREEN");
+
+        var sh1 = new Shape(red, Type.RECTANGLE);
+        var sh2 = new Shape(blue, Type.RECTANGLE);
+        var sh3 = new Shape(green, Type.CIRCLE);
+        var sh4 = new Shape(red, Type.TRIANGLE);
+
+        List.of(sh1, sh2, sh3, sh4)
+            .forEach(System.out::println);
     }
 
     @Getter
+    @ToString
     @RequiredArgsConstructor
     private static class Shape {
 
@@ -19,6 +35,7 @@ class BridgeMain {
     }
 
     @Getter
+    @ToString
     @RequiredArgsConstructor
     private static class Color {
 
