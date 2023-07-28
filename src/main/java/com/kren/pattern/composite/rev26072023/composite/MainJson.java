6cd7927d5c9deccb04f13b09fc54197c37523d79
krenMaksim <krenmaba@gmail.com>
2023-07-28 14:30:14 +0300

    Variation with MainJson which does not suite well for composite pattern

--- src/main/java/com/kren/pattern/composite/rev26072023/composite/MainJson.java
+++ src/main/java/com/kren/pattern/composite/rev26072023/composite/MainJson.java
@@ -45,7 +45,11 @@ class MainJson {
 
         JsonNode jsonNode = mapper.readTree(json);
 
-        new ObjectNode(jsonNode).showPrice();
+        // new ObjectNode(jsonNode).showPrice();
+
+        // new PriceNode(jsonNode).showPrice();
+
+        new ObjectNode2(jsonNode).showPrice();
     }
 
     static interface PriceCrawler {
@@ -73,6 +77,71 @@ class MainJson {
         public void showPrice() {
             if (Objects.nonNull(price)) System.out.println("Price: " + price);
             nodes.stream().forEach(ObjectNode::showPrice);
+        }
+    }
+
+    // -------------
+
+    static interface JsonLogger {
+
+        void showPrice();
+    }
+
+    static class PriceProperty implements JsonLogger {
+        
+        private final JsonNode node;
+
+        public PriceProperty(JsonNode node) {
+            this.node = node;
+        }
+        
+        @Override
+        public void showPrice() {
+            if (node.has("price")) {
+                System.out.println("Price: " + node.get("price"));
+            }
+        }
+
+    }
+    
+    static class PriceNode extends PriceProperty {
+        
+        private List<PriceNode> nodes;
+
+        public PriceNode(JsonNode node) {
+            super(node);
+            nodes = new ArrayList<>();
+            Iterator<JsonNode> it = node.elements();
+            while (it.hasNext()) {
+                nodes.add(new PriceNode(it.next()));
+            }
+        }
+
+        @Override
+        public void showPrice() {
+            super.showPrice();
+            nodes.stream().forEach(PriceNode::showPrice);
+        }
+
+    }
+
+
+    static class ObjectNode2 {
+
+        private JsonNode json;
+
+        public ObjectNode2(JsonNode json) {
+            this.json = json;
+        }
+
+        public void showPrice() {
+            if (json.has("price")) {
+                System.out.println("Price: " + json.get("price"));
+            }
+            Iterator<JsonNode> it = json.elements();
+            while (it.hasNext()) {
+                new ObjectNode2(it.next()).showPrice();
+            }
         }
     }
 
